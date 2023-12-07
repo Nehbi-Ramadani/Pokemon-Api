ee095e172caca016adbe30f4edb4de67abe755a9
KWiefel <dev.kwiefel427@gmail.com>
2023-12-07 09:24:48 +0100

    basic structure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-import { useState } from 'react'
-import './App.scss'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
-import Pokedex from "./Pages/Pokedex/Pokedex"
-import PokemonCard from "./Pages/PokemonCard/PokemonCard"
-
-
+import "./App.scss";
+import { Route, Routes } from "react-router-dom";
+import Pokedex from "./Pages/Pokedex/Pokedex";
+import PokemonCard from "./Pages/PokemonCard/PokemonCard";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Pokedex/>} />
-      <Route path='/pokemoncard/:id' element={<PokemonCard/>} />
-      <main className=''>
-
-      </main>
+    <>
+      <Routes>
+        <Route path="/" element={<Pokedex />} />
+        <Route path="/pokemoncard/:id" element={<PokemonCard />} />
       </Routes>
-    </BrowserRouter>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,11 +1,15 @@
-import "./SearchBar.scss"
-
-
+import "./SearchBar.scss";
+import PokemonLogo from "./../../assets/img/Pokemon-Logo.png";
 
 const SearchBar = () => {
-    return ( 
-        <div></div>
-    );
-}
+  return (
+    <>
+      <div className="logo__wrapper">
+        <img src={PokemonLogo} alt="" />
+      </div>
+      <nav className="searchbar__wrapper"></nav>
+    </>
+  );
+};
 
 export default SearchBar;
