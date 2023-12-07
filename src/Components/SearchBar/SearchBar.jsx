4ffcf63273693c7057e6d97d9b45d677a9622f64
KarakayaMetin8787 <karakayametin8787@gmail.com>
2023-12-07 14:02:20 +0100

    added navbear with search inputfield and darkmode function

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,14 +1,31 @@
 import "./SearchBar.scss";
 import PokemonLogo from "./../../assets/img/Pokemon-Logo.png";
+import { useState } from "react";
+import DarkmodeButton from "./../../assets/img/lightIcon.svg"
 
 const SearchBar = () => {
+
+  const [searchInput, setSearchInput] = useState("");
+  const [darkmode, setDarkmode] = useState(false)
+
+console.log(searchInput);
+
   return (
-    <>
-      <div className="logo__wrapper">
+    <header className={darkmode ? "darkmode" : ""}>
+      <div className="logo__wrapper" onClick={() => setDarkmode(!darkmode)}>
         <img src={PokemonLogo} alt="" />
       </div>
-      <nav className="searchbar__wrapper"></nav>
-    </>
+      <nav className="searchbar__wrapper">
+        <div className="backIcon">
+        </div>
+        <form>
+          <input type="text" onChange={(event) => setSearchInput(event.target.value)}/>
+        </form>
+        <div className="darkmodeButton">
+          <img src={DarkmodeButton} alt="button to switch darkmode on" />
+        </div> 
+      </nav>
+    </header>
   );
 };
 
